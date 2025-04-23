0bea539773ac7aafaec10d474c0e5469449817b1
KChoi0720 <choicanlight@gmail.com>
2025-04-23 22:43:26 +1200

    create new lightning form

--- force-app/main/default/lwc/lightningRecordForm/lightningRecordForm.js
+++ force-app/main/default/lwc/lightningRecordForm/lightningRecordForm.js
@@ -1,3 +1,30 @@
 import { LightningElement } from 'lwc';
 
-export default class LightningRecordForm extends LightningElement {}
+export default class CustomForm extends LightningElement {
+    name = '';
+    email = '';
+    phone = '';
+
+    handleNameChange(event) {
+        this.name = event.target.value;
+    }
+
+    handleEmailChange(event) {
+        this.email = event.target.value;
+    }
+
+    handlePhoneChange(event) {
+        this.phone = event.target.value;
+    }
+
+    handleSubmit() {
+        // You can replace this with Apex callout or validation
+        console.log('Submitted:', {
+            Name: this.name,
+            Email: this.email,
+            Phone: this.phone
+        });
+
+        alert(`Submitted:\nName: ${this.name}\nEmail: ${this.email}\nPhone: ${this.phone}`);
+    }
+}

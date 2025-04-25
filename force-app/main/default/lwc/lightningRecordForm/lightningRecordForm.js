54550d5d311c413d261c0d5ff7cdfbbe1dab636a
KChoi0720 <choicanlight@gmail.com>
2025-04-25 12:54:54 +1200

    create one custom form

---
+++ force-app/main/default/lwc/lightningRecordForm/lightningRecordForm.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class LightningRecordForm extends LightningElement {
+    fields = ['Name', 'Amount', 'ContactId', 'StageName', 'CloseDate', 'Phone', 'Email']
+    handleSuccess(event) {
+        const evt = new ShowToastEvent({
+            title: "Success!",
+            message: "Record has been saved successfully.",
+            variant: "success"
+        });
+        this.dispatchEvent(evt);
+    }
+}

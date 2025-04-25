4c330e04b529fe6d6a1fae0542b8a20a84a6d040
KChoi0720 <choicanlight@gmail.com>
2025-04-25 13:57:28 +1200

    change form name and modify cusotmization

--- force-app/main/default/lwc/lightningRecordForm/lightningRecordForm.js
+++ force-app/main/default/lwc/lightningRecordForm/lightningRecordForm.js
@@ -1,9 +1,9 @@
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-
 export default class LightningRecordForm extends LightningElement {
-    fields = ['Name', 'Amount', 'ContactId', 'StageName', 'CloseDate', 'Phone', 'Email']
+    fields = ['Name', 'Amount', 'ContactId', 'StageName', 'CloseDate', 'Phone', 'Email'];
+
     handleSuccess(event) {
         const evt = new ShowToastEvent({
             title: "Success!",
@@ -12,5 +12,14 @@ export default class LightningRecordForm extends LightningElement {
         });
         this.dispatchEvent(evt);
     }
-}
 
+    handleCancel() {
+        // Define your cancel logic here (e.g., navigate away or reset the form)
+        console.log('Cancel clicked');
+    }
+
+    handleSave() {
+        // Handle save logic if necessary
+        console.log('Save clicked');
+    }
+}
